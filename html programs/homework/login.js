5553c55f4fa4ef7892ccef9530078bca9ec71cb3
lanbaiwan <haidaidai666@gmail.com>
2024-03-31 15:57:28 +0800

    myhomework

---
+++ html programs/homework/login.js
@@ -0,0 +1,191 @@
+var isLogin=false;
+var username="";
+
+
+function setCookie(cname,cvalue,exdays){
+	var d = new Date();
+	d.setTime(d.getTime()+(exdays*1000));
+	var expires = "expires="+d.toGMTString();
+	document.cookie = cname+"="+cvalue+"; "+expires;
+}
+function getCookie(cname){
+	var name = cname + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0; i<ca.length; i++) {
+		var c = ca[i].trim();
+		if (c.indexOf(name)==0) { return c.substring(name.length,c.length); }
+	}
+	return "";
+}
+
+
+if(getCookie("username")){
+  isLogin=true;
+}
+username=getCookie("username");
+
+function checkLogin() { 
+	var name=document.getElementById("name_field").value;
+	var pass=document.getElementById("password_field").value;
+	if ((name==="lbw" && pass==="123456")||(name==="xln" && pass==="123456")||(name==="kb" && pass==="123456")
+  ||(name==="tlx" && pass==="123456")||(name==="pkq" && pass==="123456")||(name==="temper" && pass==="123456")||(name==="admin" && pass==="123456")) {  
+		const dialog = document.getElementById('dialog-success');
+    const cancelBtn = document.getElementById('cancelBtn');
+    const confirmBtn = document.getElementById('confirmBtn');
+    dialog.showModal();
+    cancelBtn.addEventListener('click', () => {
+      dialog.close();
+    });
+    confirmBtn.addEventListener('click', () => {
+      dialog.close();
+      isLogin=true;
+      setCookie("username",name,30);
+      window.location.href="home.html"; 
+    });
+	}
+	else{
+		const dialog = document.getElementById('dialog-failed');
+    const confirmBtn = document.getElementById('confirmBtn');
+    dialog.showModal();
+    confirmBtn.addEventListener('click', () => {
+      dialog.close();
+    });
+	}
+}
+function headOutput(){
+  if (isLogin ===  false) {
+    var button = document.createElement("button");
+    button.textContent = "登录";
+    button.className = "nes-btn";
+    button.onclick = function() {
+      window.location.href = "login.html";
+    };
+    document.getElementById("content").appendChild(button);
+    
+  } else {
+    var para = document.createElement("p");
+    para.textContent = "欢迎:" + username; 
+    document.getElementById("content").appendChild(para); 
+  }
+}
+
+function helpOutput(){
+  if (isLogin ===  false) {
+    var para = document.createElement("span");
+    para.className="nes-text is-success";
+    para.textContent = "点击左侧按钮即可查询对应菜系美食 点击上方登录按钮登录"; 
+    document.getElementById("help").appendChild(para); 
+    
+  
+  } else {
+    var para = document.createElement("span");
+    para.className="nes-text is-success";
+    para.textContent = "点击左侧按钮即可查询对应菜系美食"; 
+    document.getElementById("help").appendChild(para); 
+  }
+}
+
+
+function gangyueOutput(){
+  if (isLogin ===  false) {
+    const dialog = document.getElementById('dialog-islogin');
+    const confirmBtn = document.getElementById('confirmBtn');
+    dialog.showModal();
+    confirmBtn.addEventListener('click', () => {
+      dialog.close();
+    });
+  } else {
+    window.location.href = "/html programs/homework/region/gangyue.html";
+  }
+}
+
+function jiangzhehuOutput(){
+  if (isLogin ===  false) {
+    const dialog = document.getElementById('dialog-islogin');
+    const confirmBtn = document.getElementById('confirmBtn');
+    dialog.showModal();
+    confirmBtn.addEventListener('click', () => {
+      dialog.close();
+    });
+  } else {
+    window.location.href = "/html programs/homework/region/jiangzhehu.html";
+  }
+}
+
+function chuanxiangOutput(){
+  if (isLogin ===  false) {
+    const dialog = document.getElementById('dialog-islogin');
+    const confirmBtn = document.getElementById('confirmBtn');
+    dialog.showModal();
+    confirmBtn.addEventListener('click', () => {
+      dialog.close();
+    });
+  } else {
+    window.location.href = "/html programs/homework/region/chuanxiang.html";
+  }
+}
+
+function lucaiOutput(){
+  if (isLogin ===  false) {
+    const dialog = document.getElementById('dialog-islogin');
+    const confirmBtn = document.getElementById('confirmBtn');
+    dialog.showModal();
+    confirmBtn.addEventListener('click', () => {
+      dialog.close();
+    });
+  } else {
+    window.location.href = "/html programs/homework/region/lucai.html";
+  }
+}
+
+function dongbeiOutput(){
+  if (isLogin ===  false) {
+    const dialog = document.getElementById('dialog-islogin');
+    const confirmBtn = document.getElementById('confirmBtn');
+    dialog.showModal();
+    confirmBtn.addEventListener('click', () => {
+      dialog.close();
+    });
+  } else {
+    window.location.href = "/html programs/homework/region/dongbei.html";
+  }
+}
+
+function xibeiOutput(){
+  if (isLogin ===  false) {
+    const dialog = document.getElementById('dialog-islogin');
+    const confirmBtn = document.getElementById('confirmBtn');
+    dialog.showModal();
+    confirmBtn.addEventListener('click', () => {
+      dialog.close();
+    });
+  } else {
+    window.location.href = "/html programs/homework/region/xibei.html";
+  }
+}
+
+function yazhouOutput(){
+  if (isLogin ===  false) {
+    const dialog = document.getElementById('dialog-islogin');
+    const confirmBtn = document.getElementById('confirmBtn');
+    dialog.showModal();
+    confirmBtn.addEventListener('click', () => {
+      dialog.close();
+    });
+  } else {
+    window.location.href = "/html programs/homework/region/yazhou.html";
+  }
+}
+
+function oumeiOutput(){
+  if (isLogin ===  false) {
+    const dialog = document.getElementById('dialog-islogin');
+    const confirmBtn = document.getElementById('confirmBtn');
+    dialog.showModal();
+    confirmBtn.addEventListener('click', () => {
+      dialog.close();
+    });
+  } else {
+    window.location.href = "/html programs/homework/region/oumei.html";
+  }
+}
---
+++ html programs/homework/region/xibei.js
@@ -0,0 +1,140 @@
+var username="";
+let restraurant=[
+    {id:"deyuanniuroumian",chineseid:"德元牛肉面",score:[0,0,0,0,0,0,0],average:0,location:"徐汇区桂平路329-1号上海桂林科技园裙楼沿街商铺1楼"},
+    {id:"mantuoling",chineseid:"漫驼铃",score:[0,0,0,0,0,0,0],average:0,location:"普陀区长风新村街道中山北路3300号月星环球港3楼3011室"},
+    {id:"jianglijiangwai",chineseid:"疆里疆外",score:[0,0,0,0,0,0,0],average:0,location:"普陀区曹杨路458号"}
+]
+
+for(var i=0;i<restraurant.length;i++){
+    restraurant[i].score[0]=parseInt(localStorage.getItem("tlx"+restraurant[i].id));
+    restraurant[i].score[1]=parseInt(localStorage.getItem("admin"+restraurant[i].id));
+    restraurant[i].score[2]=parseInt(localStorage.getItem("pkq"+restraurant[i].id));
+    restraurant[i].score[3]=parseInt(localStorage.getItem("kb"+restraurant[i].id));
+    restraurant[i].score[4]=parseInt(localStorage.getItem("temper"+restraurant[i].id));
+    restraurant[i].score[5]=parseInt(localStorage.getItem("lbw"+restraurant[i].id));
+    restraurant[i].score[6]=parseInt(localStorage.getItem("xln"+restraurant[i].id));
+    restraurant[i].average=parseInt(localStorage.getItem("average"+restraurant[i].id));
+}
+restraurant.sort(function(a,b){
+                
+    if(a.average==b.average){
+    return b.id - a.id;
+    }
+    return b.average - a.average;
+})
+
+
+function getCookie(cname){
+	var name = cname + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0; i<ca.length; i++) {
+		var c = ca[i].trim();
+		if (c.indexOf(name)==0) { return c.substring(name.length,c.length); }
+	}
+	return "";
+}
+if(getCookie("username")){
+  isLogin=true;
+}
+username=getCookie("username");
+function headOutput(){
+    var para = document.createElement("p");
+    para.textContent = "欢迎:" + username; 
+    document.getElementById("content").appendChild(para); 
+}
+
+function bodyOutput1(){
+    var title1 = document.createElement("span");
+    title1.className="title";
+    title1.textContent = restraurant[0].chineseid; 
+    document.getElementById("title1").appendChild(title1); 
+    var s=0;
+    var len=7;
+    for(i=0;i<7;i++){
+        if(restraurant[0].score[i]) s+=restraurant[0].score[i];  
+        else len--;
+    }
+    s/=len;
+    s = s.toFixed(1);
+    localStorage.setItem("average"+restraurant[0].id, s);
+    var score = document.createElement("span");
+    score.textContent = "评分： " + s; 
+    document.getElementById("score1").appendChild(score); 
+    var l = document.createElement("span");
+    l.textContent = "地理位置： " + restraurant[0].location; 
+    document.getElementById("location1").appendChild(l); 
+}
+function reScore1(){
+    var score=document.getElementById("score_field1").value;
+	const dialog = document.getElementById('dialog-success');
+    const cancelBtn = document.getElementById('cancelBtn');
+    dialog.showModal();
+    localStorage.setItem(username+restraurant[0].id, score);
+    cancelBtn.addEventListener('click', () => {
+      dialog.close();
+    });
+}
+
+function bodyOutput2(){
+    var title1 = document.createElement("span");
+    title1.className="title";
+    title1.textContent = restraurant[1].chineseid; 
+    document.getElementById("title2").appendChild(title1); 
+    var s=0;
+    var len=7;
+    for(i=0;i<7;i++){
+        if(restraurant[1].score[i]) s+=restraurant[1].score[i];  
+        else len--;
+    }
+    s/=len;
+    s = s.toFixed(1);
+    localStorage.setItem("average"+restraurant[1].id, s);
+    var score = document.createElement("span");
+    score.textContent = "评分： " + s; 
+    document.getElementById("score2").appendChild(score); 
+    var l = document.createElement("span");
+    l.textContent = "地理位置： " + restraurant[1].location; 
+    document.getElementById("location2").appendChild(l); 
+}
+function reScore2(){
+    var score=document.getElementById("score_field2").value;
+	const dialog = document.getElementById('dialog-success');
+    const cancelBtn = document.getElementById('cancelBtn');
+    dialog.showModal();
+    localStorage.setItem(username+restraurant[1].id, score);
+    cancelBtn.addEventListener('click', () => {
+      dialog.close();
+    });
+}
+
+function bodyOutput3(){
+    var title1 = document.createElement("span");
+    title1.className="title";
+    title1.textContent = restraurant[2].chineseid; 
+    document.getElementById("title3").appendChild(title1); 
+    var s=0;
+    var len=7;
+    for(i=0;i<7;i++){
+        if(restraurant[2].score[i]) s+=restraurant[2].score[i];  
+        else len--;
+    }
+    s/=len;
+    s = s.toFixed(1);
+    localStorage.setItem("average"+restraurant[2].id, s);
+    var score = document.createElement("span");
+    score.textContent = "评分： " + s; 
+    document.getElementById("score3").appendChild(score); 
+    var l = document.createElement("span");
+    l.textContent = "地理位置： " + restraurant[2].location; 
+    document.getElementById("location3").appendChild(l); 
+}
+function reScore3(){
+    var score=document.getElementById("score_field3").value;
+	const dialog = document.getElementById('dialog-success');
+    const cancelBtn = document.getElementById('cancelBtn');
+    dialog.showModal();
+    localStorage.setItem(username+restraurant[2].id, score);
+    cancelBtn.addEventListener('click', () => {
+      dialog.close();
+    });
+}
